35db277f78f9368b4243639de73b7847b2745aef
KyleChien <kylechien0@gmail.com>
2024-04-15 22:04:13 +0800

    add general keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,15 @@
 -- Select all
 vim.keymap.set("n", "<leader>a", "gg<S-v>G", { desc = "Select all" })
 
+-- Keep window centered when going up/down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste without overwriting register
+vim.keymap.set("v", "p", '"_dP')
+
 -- Buffers
 vim.keymap.set("n", "<leader>w", "<cmd>bdelete<CR>", { desc = "Delete current buffer" })
 vim.keymap.set("n", "<leader>T", "<cmd>Resurrect<CR>", { desc = "Reopen last deleted buffer" })
@@ -17,3 +26,6 @@ vim.keymap.set(
   "<cmd>Neotree focus document_symbols toggle<CR>",
   { desc = "Focus on NeoTree document symbols" }
 )
+
+-- Scope
+vim.keymap.set("n", "<leader>fs", "<cmd>Telescope scope buffers<CR>", { desc = "Scoped buffers in all tabs" })
