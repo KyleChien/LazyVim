65d5278cdd9b3e8c3b938d3dd48c45ee41065af9
KyleChien <kylechien0@gmail.com>
2024-04-27 23:46:46 +0800

    update

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- neoscroll
+-- require("neoscroll").setup({})
+
 -- scope.nvim
 require("scope").setup({})
 require("telescope").load_extension("scope")
